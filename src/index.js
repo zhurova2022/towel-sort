356c9918c4b4b357e9f21d546196111d3b4e59dc
Alisa Zhurova <zhurova2021@gmail.com>
2023-01-20 09:00:45 +0300

    docs: add code in src/index.js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ 
+  if ((matrix === undefined) || (matrix.length === 0)){
+    return [];
+  }
+  else{
+    for(let i = 0; i < matrix.length; i++){
+      if (i % 2 !== 0){
+        matrix[i].reverse();
+      }
+    }
+  } 
+
+  let arrResult = matrix.reduce(function(a,b){
+    return a.concat(b);
+  });
+
+  return arrResult;
 }
